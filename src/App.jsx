157a71bf5816ecf545a42a4a2fbe4de95baf3885
anchornavpreetkaur <anchornavpreetkaur@gmail.com>
2025-05-17 16:32:29 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// App.jsx
+import Hello from '../src/hello';
+import Bye from '../src/bye';
+
+function App() {
+  return (
+    <div>
+      <Hello />
+      <Bye />
+    </div>
+  );
+}
+
+export default App;
