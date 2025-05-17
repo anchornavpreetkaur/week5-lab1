3d73ceb27d529f604e0b1b340b025aa52f8e9b86
navpreetkaur <anchornavpreetkaur@gmail.com>
2025-05-17 19:37:25 +0530

    ASS3

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 // App.jsx
 import Hello from '../src/hello';
 import Bye from '../src/bye';
+import ASS3 from './ASS3';
 
 function App() {
   return (
     <div>
+      <ASS3/>
       <Hello />
       <Bye />
     </div>
